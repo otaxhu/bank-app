23e5d6a52ed718e093a453de30e6ae86296c450a
oscar-jesus <oscarperniamoreno@gmail.com>
2023-04-04 22:48:28 -0400

    LoginUser too return user roles, RegisterUser automatically save user role "customer"

--- internal/service/users.service.go
+++ internal/service/users.service.go
@@ -11,6 +11,7 @@ import (
 
 var (
 	ErrUserAlreadyRegistered = errors.New("user already registered")
+	ErrPasswordTooLong       = errors.New("the password is too long")
 )
 
 type UsersService interface {
@@ -34,21 +35,38 @@ func (us *usersService) RegisterUser(ctx context.Context, user *entity.UserCrede
 
 	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
 	if err != nil {
+		if err == bcrypt.ErrPasswordTooLong {
+			return ErrPasswordTooLong
+		}
 		return err
 	}
 
 	user.Password = string(hash)
-	return us.userRepo.SaveUser(ctx, user)
+
+	if err := us.userRepo.SaveUser(ctx, user); err != nil {
+		return err
+	}
+
+	u, err = us.userRepo.GetUserByEmail(ctx, user.Email)
+	if err != nil {
+		return err
+	}
+	return us.userRepo.SaveUserRole(ctx, u.Id, "customer")
 }
 
 func (us *usersService) LoginUser(ctx context.Context, user *entity.UserCredentials) (*entity.DomainUser, error) {
-	du, err := us.userRepo.GetUserByEmail(ctx, user.Email)
+	repoUser, err := us.userRepo.GetUserByEmail(ctx, user.Email)
 	if err != nil {
 		return nil, err
 	}
 
-	if err := bcrypt.CompareHashAndPassword([]byte(du.Password), []byte(user.Password)); err != nil {
+	if err := bcrypt.CompareHashAndPassword([]byte(repoUser.Password), []byte(user.Password)); err != nil {
 		return nil, err
+	}
+
+	du := &entity.DomainUser{
+		Id:    repoUser.Id,
+		Roles: repoUser.Roles,
 	}
 
 	return du, nil
