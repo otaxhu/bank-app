665f601d989d326e8b219310069677a5f92a5f3a
oscar-jesus <oscarperniamoreno@gmail.com>
2023-04-05 18:25:32 -0400

    /users/login endpoint

--- internal/api/echo/routes.go
+++ internal/api/echo/routes.go
@@ -4,4 +4,5 @@ import "github.com/labstack/echo/v4"
 
 func (a *ApiEcho) RegisterRoutes(e *echo.Echo) {
 	e.POST("/users/register", a.handlers.RegisterUser)
+	e.POST("/users/login", a.handlers.LoginUser)
 }
