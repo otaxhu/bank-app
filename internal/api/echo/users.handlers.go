119fe94691cf9350aea326f30580ac2277071470
oscar-jesus <oscarperniamoreno@gmail.com>
2023-04-05 18:27:06 -0400

    LoginUser handler

--- internal/api/echo/users.handlers.go
+++ internal/api/echo/users.handlers.go
@@ -8,38 +8,71 @@ import (
 	"github.com/otaxhu/bank-app/internal/api/dto"
 	"github.com/otaxhu/bank-app/internal/entity"
 	"github.com/otaxhu/bank-app/internal/service"
+	"github.com/otaxhu/bank-app/internal/utils/encryption"
 )
 
 type echoHandlers struct {
-	usersServ     service.UsersService
-	dataValidator *validator.Validate
+	usersServ       service.UsersService
+	dataValidator   *validator.Validate
+	encryptionUtils *encryption.EncryptionUtils
 }
 
-type responseMessage struct {
+type responseJSON struct {
 	Message string `json:"message"`
+}
+
+func newResponseJSON(msg string) *responseJSON {
+	return &responseJSON{Message: msg}
 }
 
 func (eh *echoHandlers) RegisterUser(c echo.Context) error {
 	ctx := c.Request().Context()
-	dto := &dto.RegisterUser{}
-	if err := c.Bind(dto); err != nil {
-		return c.JSON(http.StatusBadRequest, &responseMessage{Message: "Invalid Request"})
+	params := &dto.RegisterUser{}
+	if err := c.Bind(params); err != nil {
+		return c.JSON(http.StatusBadRequest, newResponseJSON(err.Error()))
 	}
-	if err := eh.dataValidator.StructCtx(ctx, dto); err != nil {
-		return c.JSON(http.StatusBadRequest, &responseMessage{Message: err.Error()})
+	if err := eh.dataValidator.StructCtx(ctx, params); err != nil {
+		return c.JSON(http.StatusBadRequest, newResponseJSON(err.Error()))
 	}
 	cred := &entity.UserCredentials{
-		Email:    dto.Email,
-		Password: dto.Password,
+		Email:    params.Email,
+		Password: params.Password,
 	}
 	if err := eh.usersServ.RegisterUser(ctx, cred); err != nil {
-		if err == service.ErrUserAlreadyRegistered {
-			return c.JSON(http.StatusConflict, &responseMessage{Message: err.Error()})
-		}
 		if err == service.ErrPasswordTooLong {
-			return c.JSON(http.StatusBadRequest, &responseMessage{Message: err.Error()})
+			return c.JSON(http.StatusBadRequest, newResponseJSON(err.Error()))
 		}
-		return c.JSON(http.StatusInternalServerError, &responseMessage{Message: err.Error()})
+		if err == service.ErrUserAlreadyRegistered {
+			return c.JSON(http.StatusConflict, newResponseJSON(err.Error()))
+		}
+		return c.JSON(http.StatusInternalServerError, newResponseJSON(err.Error()))
 	}
 	return c.NoContent(http.StatusCreated)
+}
+
+func (eh *echoHandlers) LoginUser(c echo.Context) error {
+	ctx := c.Request().Context()
+	params := &dto.LoginUser{}
+	if err := c.Bind(params); err != nil {
+		return c.JSON(http.StatusBadRequest, newResponseJSON("Invalid Request"))
+	}
+	cred := &entity.UserCredentials{
+		Email:    params.Email,
+		Password: params.Password,
+	}
+	user, err := eh.usersServ.LoginUser(ctx, cred)
+	if err != nil {
+		if err == service.ErrPasswordTooLong {
+			return c.JSON(http.StatusBadRequest, newResponseJSON(err.Error()))
+		}
+		if err == service.ErrInvalidCredentials {
+			return c.JSON(http.StatusUnauthorized, newResponseJSON(err.Error()))
+		}
+		return c.JSON(http.StatusInternalServerError, newResponseJSON(err.Error()))
+	}
+	tokenString, err := eh.encryptionUtils.NewUserJWT(user)
+	if err != nil {
+		return c.JSON(http.StatusUnauthorized, newResponseJSON(err.Error()))
+	}
+	return c.JSON(http.StatusOK, tokenString)
 }
